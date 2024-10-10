d054043abd6d8770cc487f7e144a80af94af3042
Romain04G <145463738+Romain04G@users.noreply.github.com>
2024-10-10 12:01:21 +0200

    ajout PWM complémentaires décalés avec dead time

--- Seance 1/R/NUCLEO-G474RET6-Inverter_Pinout/Core/Src/main.c
+++ Seance 1/R/NUCLEO-G474RET6-Inverter_Pinout/Core/Src/main.c
@@ -128,6 +128,8 @@ int main(void)
 	int		 	argc = 0;
 	char*		token;
 	int 		newCmdReady = 0;
+	uint32_t period = __HAL_TIM_GET_AUTORELOAD(&htim1);
+    uint32_t pulse_value = __HAL_TIM_GET_COMPARE(&htim1, TIM_CHANNEL_1);
 
   /* USER CODE END 1 */
 
@@ -212,11 +214,15 @@ int main(void)
 		    }
 		    else if (strcmp(argv[0], "start") == 0) {
 		        HAL_UART_Transmit(&huart2, (uint8_t*)powerOn, strlen(powerOn), HAL_MAX_DELAY);
+
 		        HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 		        HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
 		        HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
 		        HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
 
+		        //__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, period*0.6);  // Déphasage complet d'une demi-période
+		        // Canal 2 : PWM de 50 % de rapport cyclique, déphasé de 180° (une demi-période)
+		        //__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, (period-pulse_value)*0.6);  // Déphasage complet d'une demi-période
 
 
 		    }
@@ -433,9 +439,9 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 0;
-  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 8499;
+  htim1.Init.Prescaler = 8;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
+  htim1.Init.Period = 1023;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -460,7 +466,7 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 5099;
+  sConfigOC.Pulse = 615;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
@@ -470,16 +476,16 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_LOW;
-  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_LOW;
+  sConfigOC.Pulse = 408;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
   }
+  HAL_TIMEx_EnableDeadTimePreload(&htim1);
   sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
   sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
   sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.DeadTime = 33;
   sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
   sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
   sBreakDeadTimeConfig.BreakFilter = 0;
